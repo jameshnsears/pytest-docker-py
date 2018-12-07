4d5a354f5d6ef79e94f6e86f31d67dce4c1e48ce
James Sears <james.hn.sears@gmail.com>
2018-12-07 18:51:22 +0000

    sonar

---
+++ travis/before_script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+pip install -r requirements.txt
+flake8 src test
+pylint --rcfile=setup.cfg src test -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt
