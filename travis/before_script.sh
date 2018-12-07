2ea60a2cfe9e34d0391c7d5ba45d92b4861b5b6a
James Sears <james.hn.sears@gmail.com>
2018-12-07 19:36:17 +0000

    coverage

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -3,4 +3,4 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 flake8 src test
-pylint --rcfile=setup.cfg src test -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt
+pylint --rcfile=setup.cfg src test -r n --msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}' > pylint-report.txt
